6eb8084700f931c069bf725fa827729c22d81df7
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-02 18:29:28 -0300

    js final

--- main.js
+++ main.js
@@ -9,81 +9,17 @@ toggleBtn.onclick = function () {
     toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
 }
 
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
-    });
-
-    const sections = $('section');
-    const navItems = $('.nav-item');
-
-    $(window).on('scroll', function () {
-        const header = $('header');
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
-        let activeSectionIndex = 0;
-
-        if (scrollPosition <= 0) {
-            header.css('box-shadow', 'none');
-        } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
-        }
-
-        sections.each(function(i) {
-            const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerHeight();
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeSectionIndex = i;
-                return false;
-            }
-        })
-
-        navItems.removeClass('active');
-        $(navItems[activeSectionIndex]).addClass('active');
-    });
-
-    ScrollReveal().reveal('#cta', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
-
-    ScrollReveal().reveal('.dish', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
-
-    ScrollReveal().reveal('#testimonial_chef', {
-        origin: 'left',
-        duration: 1000,
-        distance: '20%'
-    })
-
-    ScrollReveal().reveal('.feedback', {
-        origin: 'right',
-        duration: 1000,
-        distance: '20%'
-    })
-});
-
 window.addEventListener('scroll', function() {
-    const menu = document.getElementById('header');
+    const header = document.getElementById('header');
     
-    // Calcula o valor da opacidade conforme o scroll
     let scrollValue = window.scrollY;
-
+    
+    // Se o scroll for maior que 100px
     if (scrollValue > 100) {
-        // Quando o scroll for maior que 100px, diminui a opacidade
-        menu.style.background = 'linear-gradient(0deg, rgba(121, 0, 255, 0.8), rgba(0, 0, 0, 0.8))';
+        header.style.backgroundColor = 'rgba(119, 0, 255, 0.65)'; // Altera a transparência
     } else {
-        // Mantém a opacidade inicial, e ajusta conforme o scroll
-        let opacity = 0.2 + scrollValue / 500; // Aumenta a opacidade gradualmente
-        opacity = opacity > 0.8 ? 0.8 : opacity; // Limita a opacidade em 0.8
-        menu.style.background = `linear-gradient(0deg, rgba(121, 0, 255, ${opacity}), rgba(0, 0, 0, ${opacity}))`;
+        header.style.backgroundColor = 'rgba(119, 0, 255, 0.9)'; // Transparência inicial
     }
 });
+
 
